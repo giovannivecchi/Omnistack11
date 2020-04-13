469653349f32e78e4bef7057916e2bfa611f0a68
Giovanni Vecchi <giovanniovecchi@gmail.com>
2020-04-12 23:19:04 -0300

    Criado Métodos para a aplicação de ongs, database, e migration.

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const routes = express.Router();
+const ongController = require("./controllers/ongController");
+const IncidentController = require("./controllers/IncidentController");
+const profileController = require("./controllers/profileController");
+const sessionController = require("./controllers/sessionController");
+
+
+routes.get("/ongs", ongController.index);
+routes.post("/ongs", ongController.create);
+
+routes.post("/incidents", IncidentController.create);
+routes.get("/incidents", IncidentController.index);
+
+routes.delete("/incidents/:id", IncidentController.delete);
+
+routes.get("/profile", profileController.index);
+
+routes.post('/session', sessionController.create)
+module.exports = routes;
